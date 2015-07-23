97752dcd75931c9ae9011a29c9c1f85c6237bca9
Kelvin Kong <kelvin86@gmail.com>
2015-07-23 09:35:11 +0000

    Added contacts route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
 
+  resources :contacts
+
   get '/about' => 'pages#about'
 
   # You can have the root of your site routed with "root"
