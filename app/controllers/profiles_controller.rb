0def4bdfb56b482829de53147fbc05d248890581
Kelvin Kong <kelvin86@gmail.com>
2015-07-29 11:11:30 +0000

    Added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,10 @@
 class ProfilesController < ApplicationController
-    
+    def new 
+       #form where a user can fill out their OWN profile
+       @user = User.find(params[:user_id])
+       @variable = params[:hello]
+       @profile = @user.build_profile #method is available because profile is a nested resource in users (routes file)
+        
+    end
     
 end
