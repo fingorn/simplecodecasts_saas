0f8ead30aca4e54c63fadfed4a67a64b74d2f21a
Kelvin Kong <kelvin86@gmail.com>
2015-07-29 12:06:28 +0000

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,9 +2,28 @@ class ProfilesController < ApplicationController
     def new 
        #form where a user can fill out their OWN profile
        @user = User.find(params[:user_id])
-       @variable = params[:hello]
        @profile = @user.build_profile #method is available because profile is a nested resource in users (routes file)
         
     end
     
-end
+    def create
+        @user = User.find(params[:user_id])
+        @profile = @user.build_profile(profile_params)
+        if @profile.save
+            flash[:success] = "Profile Updated"
+           redirect_to user_path(params[:user_id])
+        else
+            render action: :new #render the new action in this controller
+        end
+        
+    end
+    
+    
+    private
+        def profile_params
+           params.require(:profile).permit(:first_name,:last_name,:job_title,:phone_number,:contact_email,:description) 
+            
+        end
+    
+    
+end 
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ActiveRecord::Base
     belongs_to :user
+    validates :first_name, :last_name, presence: true
     
     
 end
