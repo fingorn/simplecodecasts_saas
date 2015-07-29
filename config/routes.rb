4b3461e6925481feb4feba6590f2aa4503527888
Kelvin Kong <kelvin86@gmail.com>
2015-07-29 10:21:25 +0000

    Added controller and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :users do
+    resource :profile
+  end
 
   resources :contacts
 
