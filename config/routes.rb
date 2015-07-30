2aeac029cb1a26981127a780382e02609039bfde
Kelvin Kong <kelvin86@gmail.com>
2015-07-30 11:35:26 +0000

    Added form partial and edit action for profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  #note that there is a difference between #resources and #resource. resource :profile is singular as users have only 1 profile
   resources :users do
     resource :profile
   end
